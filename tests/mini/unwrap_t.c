65168e069a833f1a4eb08956b87d9e2542873237
Viacheslav <vaceslavkozin619@gmail.com>
2022-06-26 21:08:16 +0200

    Add first test with criterion

---
+++ tests/mini/unwrap_t.c
@@ -0,0 +1,21 @@
+#include <criterion/criterion.h>
+#include <criterion/new/assert.h>
+#include <criterion/new/assert.h>
+#include <criterion/assert.h>
+#include "defer.h"
+#include "zic.h"
+
+result return_ok() {
+	ZIC_RESULT_INIT();
+
+	if (RES_IS_OK()) {
+		RET_OK();
+	}
+
+	FAIL();
+}
+
+Test(Result, ReturnOk) {
+	result r = return_ok();
+	cr_assert(eq(r, OK));
+}
