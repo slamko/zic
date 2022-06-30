ae90dcafce39a559e1ae4805afeb1541b8c204b9
Viacheslav <vaceslavkozin619@gmail.com>
2022-06-30 13:58:23 +0200

    Zic example usage

--- Makefile
+++ Makefile
@@ -6,3 +6,8 @@ SRC_TARGET=test/mini.c
 
 include tests/Makefile
 
+example-std:
+	$(CC) $(LIBS) examples/mini.c -o zic_example_std
+
+example-full-api:
+	$(CC) $(LIBS) examples/full.c -o zic_example_full_api
--- examples/mini.c
+++ examples/mini.c
@@ -1,85 +1,63 @@
+#include "zic.h"
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-#define MINI_ZIC
+DEFINE_ERROR(ERR_INVARG, 10);
+DEFINE_ERROR_MSG(ERR_TOO_LONG_STRING, 11, "Too long string");
 
-#include "matchwith.h"
-#include "ptrunwrap.h"
-#include "zic.h"
-
-result some() { RET_OK(); }
-
-/* DEFINE_ERROR(ERR_INVARG, 10); */
-
-DEFINE_ERROR_MSG(ERR_TOO_LONG_STRING, 11, "Too long string")
-
-result defer_close_file(int argc, char **argv) {
-    ZIC_RESULT_INIT()
-
-    char file[] = "file1";
-    FILE *fp;
+result file_io(int argc, char **argv) {
+    FILE *fp = NULL;
     char ch;
+    ZIC_RESULT_INIT();
 
-    UNWRAP_PTR(fp = fopen(file, "r"))
+    if (argc < 2) {
+        ERROR(ERR_INVARG);
+    }
 
-    printf("%p\n", fp);
+    UNWRAP_PTR(fp = fopen(argv[1], "r"));
 
     ch = fgetc(fp);
-    TRY(fputc(ch, stdout), DO_CLEAN(cl_fp))
+    TRY(fputc(ch, stdout), DO_CLEAN(cl_fp));
+
     CLEANUP(cl_fp, fclose(fp))
     ZIC_RETURN_RESULT()
 }
 
-result some(int arg) {
-    printf("%d", arg);
-    RET_OK()
-}
-
-result defer_free_t(int argc, char **argv) {
-    char *str = NULL;
-    FILE *fp = NULL;
-
+result string_op(int argc, char **argv) {
     ZIC_RESULT_INIT();
-    ZIC_PTR_UNWRAP_INIT();
+    char *buf = NULL;
 
-    fp = PTR_UNWRAP(fopen("file1", "r"));
-
-    if (argc != 2) {
-        ERROR_DO_CLEAN(ERR_INVARG, DO_CLEAN(close_file))
+    if (argc < 2) {
+        ERROR(ERR_INVARG);
     }
 
-    if (strnlen(argv[1], 10) >= 10) {
-        ERROR_DO_CLEAN(ERR_TOO_LONG_STRING, DO_CLEAN_ALL())
+    puts("Guess my number (0-9)");
+
+    UNWRAP_PTR(buf = calloc(sizeof(*buf), 1024));
+    TRY_PTR(fgets(buf, 1024, stdin), DO_CLEAN_ALL());
+	
+    if (strcmp(buf, "7")) {
+		PRINT_TO_STDERR("Incorrect\n");
+        DO_CLEAN_ALL();
     }
 
-    strcpy(str, argv[1]);
-    UNWRAP_DO_CLEAN_ALL(puts(str));
+    puts("Right!");
 
-    CLEANUP_ALL(free(str));
-
-    CLEANUP(close_file, fclose(fp))
-}
-
-#define CLEAN(LABEL) UNWRAP_GOTO(ZIC_RES_VAR_NAME, LABEL)
-
-result some() {
-    char soup = malloc(69);
-    ZIC_RESULT_INIT()
-
-    strncpy(soup, "privet mam", 69);
-    TRY(strcmp(soup, "alle mama"), DO_CLEAN_ALL());
-
-    CLEANUP_ALL(free(soup));
+    CLEANUP_ALL(free(buf));
+    ZIC_RETURN_RESULT();
 }
 
 int main(int argc, char **argv) {
-    ZIC_RESULT_INIT()
+    ZIC_RESULT_INIT();
 
-    TRY(defer_free_t(argc, argv),
-        CATCH(ERR_INVARG, FORMAT_ERR("NEPRAVILNOE ARGUMENT")) CATCH_SYS())
+    TRY(file_io(argc, argv), CATCH(ERR_INVARG, PRINT_ERR("Invalid argument!"));
+        CATCH_SYS());
 
-    ZIC_RETURN_RESULT()
+    TRY(string_op(argc, argv),
+        CATCH(ERR_INVARG, PRINT_ERR("Invalid argument!"); CATCH_SYS()));
+
+    ZIC_RETURN_RESULT();
 }
